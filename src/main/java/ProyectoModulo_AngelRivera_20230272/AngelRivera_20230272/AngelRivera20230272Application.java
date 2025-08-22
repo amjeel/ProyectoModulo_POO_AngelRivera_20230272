e04782f3d2514cfd1220816d983514e5f051c0c7
amjeel <aamjelmacarron@gmail.com>
2025-08-22 10:41:14 -0600

    creacion del repositorio

---
+++ src/main/java/ProyectoModulo_AngelRivera_20230272/AngelRivera_20230272/AngelRivera20230272Application.java
@@ -0,0 +1,13 @@
+package ProyectoModulo_AngelRivera_20230272.AngelRivera_20230272;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngelRivera20230272Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngelRivera20230272Application.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoModulo_AngelRivera_20230272/AngelRivera_20230272/AngelRivera20230272ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoModulo_AngelRivera_20230272.AngelRivera_20230272;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngelRivera20230272ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
